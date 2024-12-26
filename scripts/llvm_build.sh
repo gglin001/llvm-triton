560ce4e91dc6709029e732176fcc2f559869bd44
Allen Guo <guosonglin001@gmail.com>
2024-12-26 22:21:08 +0800

    add scripts (#1)

---
+++ scripts/llvm_build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rm -f $PWD/llvm-project/llvm/CMakePresets.json
+ln -s $PWD/scripts/CMakePresets.json $PWD/llvm-project/llvm/CMakePresets.json
+
+pushd llvm-project
+
+cmake --preset osx -S$PWD/llvm -B$PWD/build
+
+# TODO: reduce cmake targets, only llvm & mlir libs
+
+cmake --build $PWD/build --target all
+cmake --build $PWD/build --target install
+
+popd
+
+###############################################################################
+
+# log
+
+# cmake --build ...
+# ~ 4800 targets
+
+###############################################################################
