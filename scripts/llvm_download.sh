f5167796e9eb1076ceb153ac26620c4f92cee25f
Allen Guo <guosonglin001@gmail.com>
2024-12-26 21:48:47 +0800

    add scripts

---
+++ scripts/llvm_download.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+mkdir -p llvm-project && pushd llvm-project
+
+git init
+
+git remote add origin git@github.com:llvm/llvm-project.git
+# git remote add origin /repo/llvm-project
+
+######
+
+git sparse-checkout set --no-cone \
+  /cmake /llvm /mlir \
+  '!/llvm/test' '!/llvm/unittests' '!/llvm/docs' \
+  '!/mlir/test' '!/mlir/unittests' '!/mlir/docs' \
+  /.vscode '/*.*'
+
+git sparse-checkout add \
+  '!/llvm/utils/gn' \
+  '!/llvm/lib/Target' '/llvm/lib/Target/*.*' \
+  '/llvm/lib/Target/RISCV' '/llvm/lib/Target/ARM' '/llvm/lib/Target/AArch64' \
+  '/llvm/lib/Target/AMDGPU' 'llvm/lib/Target/NVPTX' 'llvm/lib/Target/X86'
+
+git sparse-checkout list
+
+######
+
+LLVM_SHA="86b69c31642e98f8357df62c09d118ad1da4e16a"
+
+git fetch --depth 1 origin $LLVM_SHA
+git checkout -b sync $LLVM_SHA
+
+popd
